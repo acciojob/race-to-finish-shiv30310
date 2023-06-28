25675079bd18ba73bff37f1940b154f6d0864ed3
Gulshan Soni <43094983+shiv30310@users.noreply.github.com>
2023-06-28 12:08:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 window.promises = [];
 
-// Do not change the code above this
-// add your promises to the array `promises`
+
+const output = document.getElementById("output")
+
+for(let i=1; i<6; i++){
+    promises.push(
+        new Promise(resolve => {
+        setTimeout(() =>{
+            resolve(i)
+        }, 1000*i)
+        })
+)}
+
+Promise.any(promises)
+.then((res) => {
+    output.innerHTML = res
+}).catch(error => {
+    console.log(error)
+})
